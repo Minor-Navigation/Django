ab8cdfa28b4b1e6ca4ad78d1425fca381283d8c3
Arjun Gupta <saimzn4241@gmail.com>
2017-04-20 20:40:19 +0530

    Added a Procfile.

--- new_project/settings.py
+++ new_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'mhc)cw5cq)sp1n@3srhuidlchhv=3+&(g!o*4i^y$be28-6ij2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
